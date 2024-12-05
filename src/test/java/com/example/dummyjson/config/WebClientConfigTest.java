17f3bdbcbf92655316aace865cc41c47d7c3b65f
eliascmurat <elias-murat@outlook.com>
2024-12-05 16:46:36 -0300

    refactor: trocando resttemplate por webclient

---
+++ src/test/java/com/example/dummyjson/config/WebClientConfigTest.java
@@ -0,0 +1,22 @@
+package com.example.dummyjson.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class WebClientConfigTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void testWebClientBuilderBean() {
+        WebClient.Builder webClientBuilder = context.getBean(WebClient.Builder.class);
+        assertNotNull(webClientBuilder, "The WebClient.Builder bean should be present in the context.");
+    }
+}
